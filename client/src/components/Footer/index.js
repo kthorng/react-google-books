3acde8ab540ace8137978b768e47ec403e8288fe
Keo <kthorng@gmail.com>
2019-01-12 19:15:02 -0800

    styles

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -3,8 +3,8 @@ import React from "react";
 function Footer() {
   return (
     <footer>
-      <hr />
-      <p className="pull-right">
+      <br></br>
+      <p className="pull-right footer">
         <i className="fab fa-github" /> Proudly built using React.js
       </p>
     </footer>
